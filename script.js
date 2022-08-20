c9261adf6b05bb1fcaff74e9ebd69999ac8e21f5
Diego Valles <dgvall134@gmail.com>
2022-08-20 12:37:49 -0600

    added background color change event listener

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-const passions = document.getElementById(`nav1`);
+const diego = document.getElementById(`name`);
+let i=1
 
-passions.addEventListener(`mouseover`, function() {
-    passions.style.color = `red`
+diego.addEventListener("click", function() {
+    if (i === 1) {
+    alert("More of a night mode kind of person?");
+    document.body.style.backgroundColor = "black";
+    i=2;
+} else if (i === 2) {
+    alert("More of a dark mode kind of person?");
+    document.body.style.backgroundColor = "#0B0C10";
+    i=1;
+}
 })
-passions.addEventListener(`mouseout`, function() {
-    passions.style.color = `black`
-})
+
+
 
 
 
