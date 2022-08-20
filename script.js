4c65e79b15253580203827f594a38a3cb3c2f282
Diego Valles <dgvall134@gmail.com>
2022-08-20 11:07:27 -0600

    color scheme and more styling

---
+++ script.js
@@ -0,0 +1,11 @@
+const passions = document.getElementById(`nav1`);
+
+passions.addEventListener(`mouseover`, function() {
+    passions.style.color = `red`
+})
+passions.addEventListener(`mouseout`, function() {
+    passions.style.color = `black`
+})
+
+
+
